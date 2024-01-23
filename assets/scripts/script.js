5a53540a8386b3bec27ff1c847123a386314366d
Ian Hart <ianhart7790@gmail.com>
2024-01-23 10:59:36 -0500

    display the current day, month and ordinal in the header

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,21 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+
 $(function () {
+  function getCurrentDay() {
+    var format = 'dddd, MMMM, Do';
+    return dayjs().format(format);
+  }
+
+  function displayCurrentDay() {
+    var currentDayEl = $('#currentDay');
+    currentDayEl.text(getCurrentDay());
+  }
+
+  function main() {
+    displayCurrentDay();
+  }
+
+  main();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
