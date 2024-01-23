fdb8bf96b21d7ca6ab842e2c94bcafc4e0c10a59
Ian Hart <ianhart7790@gmail.com>
2024-01-23 12:56:07 -0500

    get the user input event and save to local storage also get the saved event from local storage and set as textarea text

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,6 +16,10 @@ $(function () {
     currentDayEl.text(getCurrentDay());
   }
 
+  function getSavedEventIfExists(workDayHourId) {
+    return localStorage.getItem(workDayHourId);
+  }
+
   // calculate if the time is in the past, present, or future
   function calculateTimeType(timeBlockId) {
     var workDayHour = extractHourFromId(timeBlockId);
@@ -53,11 +57,20 @@ $(function () {
       .text(dayjs().hour(workDayHour).format('hA'));
   }
 
-  // create description element
-  function createDescriptionEl() {
+  // create description element and if there is an event that is saved to local storage use that as description text
+  function createDescriptionEl(workDayHourId) {
+    var savedEvent = getSavedEventIfExists(workDayHourId);
+
+    var text = '';
+
+    if (savedEvent !== null) {
+      text = savedEvent;
+    }
+
     return $('<textarea>')
       .addClass('col-8 col-md-10 description')
-      .attr({ rows: 3 });
+      .attr({ rows: 3 })
+      .text(text);
   }
 
   // create save button and append font awesome save icon to button
@@ -72,6 +85,7 @@ $(function () {
     return saveBtnEl;
   }
 
+  // create all the individual elements that make up the timeblock and append them to the timeblock row and then the row to the container
   function displayTimeBlocks() {
     var workDayHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
@@ -81,11 +95,19 @@ $(function () {
       rowEl.addClass(timeType);
 
       createTimeEl(rowEl.attr('id'), workDayHours).appendTo(rowEl);
-      createDescriptionEl().appendTo(rowEl);
+      createDescriptionEl(rowEl.attr('id')).appendTo(rowEl);
       createSaveBtnEl().appendTo(rowEl);
 
       rowEl.appendTo(containerEl);
     }
+  }
+
+  // if the description textarea is empty do nothing, if there is a value save to local storage
+  function saveEvent(e) {
+    var event = $(this).prev().val();
+    var key = $(this).parent().attr('id');
+
+    localStorage.setItem(key, event);
   }
 
   function main() {
@@ -94,22 +116,6 @@ $(function () {
   }
 
   main();
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  containerEl.on('click', '.saveBtn', saveEvent);
 });
